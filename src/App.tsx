59916f2aaa1527ef6642553051fffd193ee968fc
Ayan Malik <malikayan575@gmail.com>
2026-03-10 23:54:07 +0500

    Home Page Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Aurora from './components/AuroraBackground';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
 
 function App() {
   return (
